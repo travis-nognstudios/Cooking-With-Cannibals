84e0d2b36222d4c7a9f26b9780f14a307600ca9e
Red Nur <rednur01@gmail.com>
2019-10-25 00:37:36 -0400

    make hardcoded recipes work

--- Assets/_Scripts/Cooking/Cookable.cs
+++ Assets/_Scripts/Cooking/Cookable.cs
@@ -2,110 +2,117 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cookable : MonoBehaviour
+namespace Cooking
 {
-    #region Variables
-
-    public enum Cooktype { Grill, Boil, Deepfry };
-
-    [Header("Cooking Settings")]
-    public Cooktype cooktype;
-    public float timeToCook;
-    public float timeToBurn;
-
-    private float timeCooked;
-    private bool cooked;
-    private bool burnt;
-
-    [Header("Textures")]
-    public Material[] material;
-    Renderer rend;
-
-    #endregion Variables
-
-    
-    #region Properties
-
-    /// <summary>
-    /// Whether the object is raw
-    /// </summary>
-    /// <returns></returns>
-    private bool IsRaw()
+    public class Cookable : MonoBehaviour
     {
-        return !(IsCooked() || IsBurnt());
-    }
+        #region Variables
 
-    /// <summary>
-    /// Whether the object is cooked
-    /// </summary>
-    /// <returns></returns>
-    private bool IsCooked()
-    {
-        return cooked == true && burnt == false;
-    }
+        public enum Cooktype { Grill, Boil, Deepfry };
+        public enum Ingredient { Shoulder, Hand, BurgerBuns, Tongue, Lettuce };
 
-    /// <summary>
-    /// Whether the object is burnt
-    /// </summary>
-    /// <returns></returns>
-    private bool IsBurnt()
-    {
-        return burnt == true;
-    }
-    
-    #endregion Properties
+        [Header("Type")]
+        public Ingredient ingredientType;
+
+        [Header("Cooking Settings")]
+        public Cooktype cooktype;
+        public float timeToCook;
+        public float timeToBurn;
+
+        private float timeCooked;
+        private bool cooked;
+        private bool burnt;
+
+        [Header("Textures")]
+        public Material[] material;
+        Renderer rend;
+
+        #endregion Variables
 
 
+        #region Properties
 
-    #region MonoBehavior
-    // Start is called before the first frame update
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
-        rend.sharedMaterial = material[0];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        //ToDo: Add a check that cooktop's cooktype matches my cooktype
-        //Removing it for the demo
-        if (other.gameObject.CompareTag("Cooktop"))
+        /// <summary>
+        /// Whether the object is raw
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRaw()
         {
-            timeCooked += Time.deltaTime;
+            return !(IsCooked() || IsBurnt());
+        }
 
-            if (!IsCooked() && timeCooked >= timeToCook && timeCooked < timeToBurn)
+        /// <summary>
+        /// Whether the object is cooked
+        /// </summary>
+        /// <returns></returns>
+        public bool IsCooked()
+        {
+            return cooked == true && burnt == false;
+        }
+
+        /// <summary>
+        /// Whether the object is burnt
+        /// </summary>
+        /// <returns></returns>
+        public bool IsBurnt()
+        {
+            return burnt == true;
+        }
+
+        #endregion Properties
+
+
+
+        #region MonoBehavior
+        // Start is called before the first frame update
+        void Start()
+        {
+            rend = GetComponent<Renderer>();
+            rend.enabled = true;
+            rend.sharedMaterial = material[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            //ToDo: Add a check that cooktop's cooktype matches my cooktype
+            //Removing it for the demo
+            if (other.gameObject.CompareTag("Cooktop"))
             {
-                MakeCooked();
-            }
-            else if (!IsBurnt() && timeCooked >= timeToBurn)
-            {
-                MakeBurnt();
+                timeCooked += Time.deltaTime;
+
+                if (!IsCooked() && timeCooked >= timeToCook && timeCooked < timeToBurn)
+                {
+                    MakeCooked();
+                }
+                else if (!IsBurnt() && timeCooked >= timeToBurn)
+                {
+                    MakeBurnt();
+                }
             }
         }
-    }
-    #endregion MonoBehavior
+        #endregion MonoBehavior
 
-    #region Private Methods
-    private void MakeCooked()
-    {
-        cooked = true;
-        rend.sharedMaterial = material[1];
-        Debug.Log("Cooked");
-    }
+        #region Private Methods
+        private void MakeCooked()
+        {
+            cooked = true;
+            rend.sharedMaterial = material[1];
+            Debug.Log("Cooked");
+        }
 
-    private void MakeBurnt()
-    {
-        burnt = true;
-        rend.sharedMaterial = material[2];
-        Debug.Log("Burnt");
-    }
-    #endregion Private Methods
+        private void MakeBurnt()
+        {
+            burnt = true;
+            rend.sharedMaterial = material[2];
+            Debug.Log("Burnt");
+        }
+        #endregion Private Methods
 
+    }
 }
