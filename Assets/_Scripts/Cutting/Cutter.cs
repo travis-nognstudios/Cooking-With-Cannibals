c4fe153044d1984b85bbd811c739511a06638fd2
Jason <jw3345@drexel.edu>
2019-10-29 16:30:16 -0400

    Added cutting stuff

---
+++ Assets/_Scripts/Cutting/Cutter.cs
@@ -0,0 +1,20 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class Cutter : MonoBehaviour {
+
+	public Material capMaterial;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+        Destroy(pieces[1], 1);
+    }
+
+}
--- Assets/_Scripts/Cutting/Cutting.cs
+++ Assets/_Scripts/Cutting/Cutting.cs
@@ -6,15 +6,13 @@ namespace Cutting
 {
     public class Cutting : MonoBehaviour
     {
-
-        public Material CapMaterial;
-
+        public Material capMaterial;
 
         private void OnTriggerEnter(Collider collision)
-        {
+        {   
             GameObject victim = collision.gameObject;
 
-            GameObject[] pieces = MeshCut.Cut(victim, transform.position, transform.right, CapMaterial);
+            GameObject[] pieces = MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
             if (!pieces[1].GetComponent<Rigidbody>())
                 pieces[1].AddComponent<Rigidbody>();
