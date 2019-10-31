2ed1fde01f701d24256a18e3894316c9233923ba
Red Nur <rednur01@gmail.com>
2019-10-31 14:46:00 -0400

    add rating system for meals, missing a way to get meals from the world into script

---
+++ Assets/_Scripts/Serving/Mealspawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Serving
+{
+    public class Mealspawner : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
