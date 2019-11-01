254a488fcb2f52d12d06928149486f80c2e7a1c8
Jason <jw3345@drexel.edu>
2019-11-01 19:46:48 -0400

    started order spawner

---
+++ Assets/_Scripts/Serving/OrderSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RecipeManager;
+
+namespace RecipeManager
+{
+    public class OrderSpawner : MonoBehaviour
+    {
+        //array of recipes
+        public Recipes[] recipes;
+
+        //index of array
+        private int i;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            recipes[] = new RecipeManager[]; 
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            i = Random.Range(0, recipes.Length);
+            if (recipes[i] == )
+            {
+
+            }
+        }
+
+        void SpawnOrder()
+        {
+            recipes[i].
+        }
+    }
+}
