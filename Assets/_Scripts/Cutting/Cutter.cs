0dfb48e7d30382f9106afb31e3cd96db3ab51522
Wang <jw3345@drexel.edu>
2019-11-08 12:12:26 -0500

    Added recipe manager

--- Assets/_Scripts/Cutting/Cutter.cs
+++ Assets/_Scripts/Cutting/Cutter.cs
@@ -5,16 +5,19 @@ public class Cutter : MonoBehaviour {
 
 	public Material capMaterial;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        GameObject victim = collision.collider.gameObject;
+        if (collision.gameObject.tag == "Cuttable")
+        {
+            GameObject victim = collision.gameObject;
 
-        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+            GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
-        if (!pieces[1].GetComponent<Rigidbody>())
-            pieces[1].AddComponent<Rigidbody>();
+            if (!pieces[1].GetComponent<Rigidbody>())
+                pieces[1].AddComponent<Rigidbody>();
 
-        Destroy(pieces[1], 1);
+            Destroy(pieces[1], 1);
+        }
     }
 
 }
