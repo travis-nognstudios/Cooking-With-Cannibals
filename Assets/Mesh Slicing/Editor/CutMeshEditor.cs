630aff0b529b304320a08c63ab783590fa1939aa
Jason <jwang11211@gmail.com>
2019-11-13 00:10:00 -0500

    cutting wip

---
+++ Assets/Mesh Slicing/Editor/CutMeshEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CutSimpleConvex))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CutSimpleConvex myScript = (CutSimpleConvex)target;
+        if (GUILayout.Button("Cut Object"))
+        {
+            myScript.Cut();
+        }
+    }
+}
+
+[CustomEditor(typeof(CutSimpleConcave))]
+public class ObjectBuilderEditor3 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CutSimpleConcave myScript = (CutSimpleConcave)target;
+        if (GUILayout.Button("Cut Object"))
+        {
+            myScript.Cut();
+        }
+    }
+}
+
+[CustomEditor(typeof(CutMultiplePartsConcave))]
+public class ObjectBuilderEditor8 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CutMultiplePartsConcave myScript = (CutMultiplePartsConcave)target;
+        if (GUILayout.Button("Cut Object"))
+        {
+            myScript.Cut();
+        }
+    }
+}
