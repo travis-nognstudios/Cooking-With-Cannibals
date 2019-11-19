5d0e9f066c27ffe6a7b53f75904643b286cccbc0
Nur <zn32@drexel.edu>
2019-11-19 15:45:53 -0500

    add basic spawning without max check

---
+++ Assets/_Scripts/Supplies/SupplyManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Supplies
+{
+    public class SupplyManager : MonoBehaviour
+    {
+        public int spawnInterval = 5;
+        public SupplyPoint[] supplyPoints;
+
+        private float sinceLastSpawn = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            sinceLastSpawn += Time.deltaTime;
+
+            if (sinceLastSpawn >= spawnInterval)
+            {
+                sinceLastSpawn = 0;
+                SpawnItems();
+            }
+
+        }
+
+        void SpawnItems()
+        {
+            foreach(SupplyPoint point in supplyPoints)
+            {
+                // ToDo: Check how many are already in supplyarea
+                GameObject item = point.object_to_spawn;
+                Vector3 pos = point.spawnArea.transform.position;
+
+                GameObject clone = Instantiate(item);
+                clone.transform.position = pos;
+                Debug.Log("Spawned " + item.name);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Supplies/SupplyPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Supplies
+{
+    [System.Serializable]
+    public struct SupplyPoint
+    {
+        public GameObject object_to_spawn;
+        public GameObject spawnArea;
+        public int maxSpawnNumber;
+    }
+}
