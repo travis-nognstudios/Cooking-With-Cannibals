c4c6790bd8dc68cdd26b081c1ede6c331a698b26
Travis Hove <hove.travis@gmail.com>
2019-12-04 20:07:34 +0000

    Added a bunch of audio to falling object
    Still need to troubleshoot making noises on objects held in the player's hand

---
+++ Assets/_Scripts/Audio/ImpactEffect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace Audio
+{
+    public class ImpactEffect : VRTK.VRTK_InteractableObject
+    {
+        public float dropSpeed = 1f;
+
+       public override void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
+        {
+            base.OnInteractableObjectGrabbed(e);
+            Debug.Log("Im Grabbed");
+            GetComponent<GrabBasedAudio>().SetInteractSound();
+        }
+
+
+        public override void OnInteractableObjectUngrabbed(InteractableObjectEventArgs e)
+        {
+            base.OnInteractableObjectUngrabbed(e);
+            Debug.Log("Not grabbed");
+            GetComponent<GrabBasedAudio>().SetDropSound();
+        }
+
+        public virtual void Grabbed(VRTK_InteractGrab other)
+        {
+
+            Debug.Log("Im Grabbed method");
+            GetComponent<GrabBasedAudio>().SetInteractSound();
+        }
+
+
+        public virtual void Ungrabbed(VRTK_InteractGrab other)
+        {
+            Debug.Log("Not grabbed method");
+            GetComponent<GrabBasedAudio>().SetDropSound();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.relativeVelocity.magnitude > dropSpeed)
+            {
+                GetComponent<GrabBasedAudio>().soundSource.Play();
+                Debug.Log("Something should be playing");
+            }
+        }
+    }
+}
