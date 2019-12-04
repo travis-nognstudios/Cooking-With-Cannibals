9112ccf8d78d0290e0ef0f7753dbac0fea741a97
Travis Hove <hove.travis@gmail.com>
2019-12-04 20:07:34 +0000

    Added a bunch of audio to falling object
    Still need to troubleshoot making noises on objects held in the player's hand

---
+++ Assets/GrabBasedAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GrabBasedAudio : MonoBehaviour
+{
+
+    public AudioSource soundSource;
+    public AudioClip dropSound;
+    public AudioClip interactSound;
+    public float dropSpeed = 1f;
+    private bool isGrabbed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSource = this.GetComponent<AudioSource>();
+        soundSource.clip = dropSound;
+        Debug.Log("Start works");
+    }
+
+    public void SetInteractSound()
+    {
+        soundSource.clip = interactSound;
+    }
+
+    public void SetDropSound()
+    {
+        soundSource.clip = dropSound;
+    }
+}
