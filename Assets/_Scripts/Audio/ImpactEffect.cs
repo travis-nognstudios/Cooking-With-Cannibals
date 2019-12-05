98dee26ce2492b541082d93d161945a671642e20
Red Nur <rednur01@gmail.com>
2019-12-04 20:38:36 -0500

    orderspawner works

--- Assets/_Scripts/Audio/ImpactEffect.cs
+++ Assets/_Scripts/Audio/ImpactEffect.cs
@@ -14,7 +14,7 @@ namespace Audio
         public override void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
         {
             base.OnInteractableObjectGrabbed(e);
-            Debug.Log("Im Grabbed");
+            // Debug.Log("Im Grabbed");
             GetComponent<GrabBasedAudio>().SetInteractSound();
         }
 
@@ -22,21 +22,21 @@ namespace Audio
         public override void OnInteractableObjectUngrabbed(InteractableObjectEventArgs e)
         {
             base.OnInteractableObjectUngrabbed(e);
-            Debug.Log("Not grabbed");
+            // Debug.Log("Not grabbed");
             GetComponent<GrabBasedAudio>().SetDropSound();
         }
 
         public virtual void Grabbed(VRTK_InteractGrab other)
         {
 
-            Debug.Log("Im Grabbed method");
+            // Debug.Log("Im Grabbed method");
             GetComponent<GrabBasedAudio>().SetInteractSound();
         }
 
 
         public virtual void Ungrabbed(VRTK_InteractGrab other)
         {
-            Debug.Log("Not grabbed method");
+            // Debug.Log("Not grabbed method");
             GetComponent<GrabBasedAudio>().SetDropSound();
         }
 
@@ -45,7 +45,7 @@ namespace Audio
             if (other.relativeVelocity.magnitude > dropSpeed)
             {
                 GetComponent<GrabBasedAudio>().soundSource.Play();
-                Debug.Log("Something should be playing");
+                // Debug.Log("Something should be playing");
             }
         }
     }
