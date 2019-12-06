2c158e8a4ba16956c6d2fb30f0d70d356f21e0aa
Jason <jwang11211@gmail.com>
2019-12-06 08:17:21 -0500

    more interactables, highlights

--- Assets/_Scripts/LightSwitch.cs
+++ Assets/_Scripts/LightSwitch.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class LightSwitch : MonoBehaviour
 {
     public bool isOn = false;
+    bool isTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!isOn)
+        if (!isTriggered)
         {
-            isOn = true;
-            transform.position = new Vector3(-0.4937657f, -0.1230854f, 0);
-            transform.Rotate(0, 0, -21.76f, Space.Self);
-        }
-        else
-        {
-            transform.position = new Vector3(0, 0, 0);
-            transform.Rotate(0, 0, 21.76f, Space.Self);
-        }
+            isTriggered = true;
+            if (!isOn)
+            {
+                isOn = true;
+                transform.position = new Vector3(-0.4937657f, -0.1230854f, 0);
+                transform.Rotate(0, 0, -21.76f, Space.Self);
+            }
+            else
+            {
+                isOn = false;
+                transform.position = new Vector3(0, 0, 0);
+                transform.Rotate(0, 0, 21.76f, Space.Self);
+            }
+        }   
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (isTriggered)
+            isTriggered = false;
     }
 }
