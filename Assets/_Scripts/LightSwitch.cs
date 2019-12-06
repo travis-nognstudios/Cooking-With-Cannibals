6e3fa0f9602740d6a77854f39e208e0dbf287439
Jason <jwang11211@gmail.com>
2019-12-06 07:39:35 -0500

    interactables

---
+++ Assets/_Scripts/LightSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public bool isOn = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isOn)
+        {
+            isOn = true;
+            transform.position = new Vector3(-0.4937657f, -0.1230854f, 0);
+            transform.Rotate(0, 0, -21.76f, Space.Self);
+        }
+        else
+        {
+            transform.position = new Vector3(0, 0, 0);
+            transform.Rotate(0, 0, 21.76f, Space.Self);
+        }
+    }
+}
