561f2c72b1f08b80a5522de2a10dc7f70cb44cb4
Nur <zn32@drexel.edu>
2020-01-29 20:36:32 -0500

    add two finger grabbing

---
+++ Assets/_Scripts/GrabWithBothFingers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GrabWithBothFingers : VRTK_ControllerEvents
+{
+    // Keep track of whether grip is pressed because
+    // of real grip press or alias = trigger press
+    private bool triggerIsGrabbing;
+    
+    public override void OnTriggerPressed(ControllerInteractionEventArgs e)
+    {
+        if (!gripPressed)
+        {
+            base.OnGripPressed(e);
+        }
+
+    }
+    
+    public override void OnTriggerReleased(ControllerInteractionEventArgs e)
+    {
+        OnGripReleased(e);
+    }
+
+    public override void OnGripReleased(ControllerInteractionEventArgs e)
+    {
+        if (!gripPressed && !triggerPressed)
+        {
+            base.OnGripReleased(e);
+        }
+    }
+
+}
