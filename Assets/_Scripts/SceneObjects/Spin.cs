ba43d6a3163ecb3343b7925f6c06918ad050aaf2
Suarez <zjs35@drexel.edu>
2020-01-31 13:30:56 -0500

    working fridge door, garbage can and draw, and fridge fan spinning

---
+++ Assets/_Scripts/SceneObjects/Spin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+
+    void Update()
+    {
+        transform.Rotate(x, y, z);
+    }
+}
