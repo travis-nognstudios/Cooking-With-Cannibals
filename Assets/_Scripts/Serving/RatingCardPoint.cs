d390e2422d01fd9aafe6dd6ba34a513925656837
Red Nur <rednur01@gmail.com>
2020-02-02 15:34:35 -0500

    add rating cards at end of level

---
+++ Assets/_Scripts/Serving/RatingCardPoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using AI;
+
+namespace Serving
+{
+    [System.Serializable]
+    public class RatingCardPoint : MonoBehaviour
+    {
+        private GameObject cardReference;
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public void SetCard(GameObject card)
+        {
+            // Instantiate card object
+            // Create spring joint to spawn point
+            Vector3 offset = new Vector3(0, -0.1f, 0);
+            Vector3 position = transform.position + offset;
+            Quaternion rotation = card.transform.rotation;
+
+            GameObject createdCard = Instantiate(card, position, rotation);
+            createdCard.GetComponent<SpringJoint>().connectedBody = GetComponent<Rigidbody>();
+
+            cardReference = createdCard;
+        }
+
+        public void DestroyCard()
+        {
+            if (cardReference != null)
+            {
+                Destroy(cardReference);
+            }
+        }
+
+    }
+}
---
+++ Assets/_Scripts/Serving/RatingCardSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Serving
+{
+    public class RatingCardSpawner : MonoBehaviour
+    {
+        [Header("Rating Cards")]
+        public GameObject bestRatingCard;
+        public GameObject goodRatingCard;
+        public GameObject failRatingCard;
+
+        [Header("Ratings By Tip")]
+        public int perfectIfAboveTip;
+        public int failIfBelowTip;
+
+        [Header("Scene Objects")]
+        public TipJar tipjar;
+        public RatingCardPoint cardPoint;
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SpawnCard()
+        {
+            GameObject selectedCard;
+            int tip = tipjar.GetAmountInJar();
+
+            if (tip >= perfectIfAboveTip)
+            {
+                selectedCard = bestRatingCard;
+            }
+            else if (tip <= failIfBelowTip)
+            {
+                selectedCard = failRatingCard;
+            }
+            else
+            {
+                selectedCard = goodRatingCard;
+            }
+
+            cardPoint.SetCard(selectedCard);
+        }
+    }
+}
