ba6ef126bff7732339ef46e7728f367ad049af30
Nur <zn32@drexel.edu>
2020-02-10 10:43:00 -0500

    stove turners snapping

---
+++ Assets/_Scripts/SceneObjects/StoveTurner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+namespace SceneObjects
+{
+    public class StoveTurner : VRTK_InteractableObject
+    {
+        public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject = null)
+        {
+            Debug.Log("Stopped grabbing stove turner");
+            Debug.Log($"Current Rotation: {transform.localRotation}");
+
+            
+            if (transform.rotation.z > 0.3)
+            {
+                SnapToOn();
+            }
+            else
+            {
+                SnapToOff();
+            }
+            
+
+            base.Ungrabbed(previousGrabbingObject);
+        }
+
+        public void SnapToOff()
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 1.0f);
+        }
+
+        public void SnapToOn()
+        {
+            transform.rotation = new Quaternion(0, 0, 0.7f, 0.7f);
+        }
+    }
+}
