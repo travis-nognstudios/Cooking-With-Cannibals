6f5a858972b3aa6564d4006f79df1d7a8b5e08db
Travis Hove <hove.travis@gmail.com>
2020-02-28 00:52:56 -0500

    week 8 promo ready for build

---
+++ Assets/_Scripts/PropDestruction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropDestruction : MonoBehaviour
+{
+    public GameObject breakVersion;
+    public float bForce = 1f;
+    protected Rigidbody rb;
+    private int active = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(rb.velocity.magnitude > bForce && active==0)
+        {
+            active++;
+            Instantiate(breakVersion, transform.position, new Quaternion(0,90,90,0));
+            //explosion for geo breakables
+            //rb.AddExplosionForce(10f, Vector3.zero, 0f);
+            Destroy(gameObject);
+        }
+    }
+}
