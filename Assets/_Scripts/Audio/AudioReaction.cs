c7bf4fe8c5ecf383d865f70d2ba267cfaa9186f0
Jason <jwang11211@gmail.com>
2020-03-05 00:52:33 -0500

    ear anim/arm dodge

---
+++ Assets/_Scripts/Audio/AudioReaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReaction : MonoBehaviour
+{
+    
+    public AudioSource[] sounds;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //sounds = FindObjectsOfType<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].isPlaying)
+            {
+                //Debug.Log("AudioReaction.cs: playing");
+
+                anim.SetTrigger("EarTrig");
+            }
+        }
+    }
+}
