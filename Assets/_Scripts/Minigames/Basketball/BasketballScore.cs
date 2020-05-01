7e26946e0d11595eae7a769472eb5234f2846333
Jason <jwang11211@gmail.com>
2020-04-30 22:09:52 -0400

    minigames!

---
+++ Assets/_Scripts/Minigames/Basketball/BasketballScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BasketballScore : MonoBehaviour
+{
+    public TMP_Text text;
+    private float score;
+    private AudioSource scoreSound;
+    public ParticleSystem scoreParticle;
+    void Start()
+    {
+        score = 0;
+        scoreSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            score++;
+            text.text = score.ToString();
+            scoreSound.Play();
+            scoreParticle.Play();
+            other.tag = "Untagged";
+        }
+    }
+}
---
+++ Assets/_Scripts/Minigames/Bowling/BowlingScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BowlingScore : MonoBehaviour
+{
+    //public TextMeshProUGUI scoreText;
+    private int temp = 0;
+    private int score = 0;
+    //public BottleSpawner spawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pin"))
+        {
+            temp++;
+            //Destroy(other);
+            Debug.Log(temp);
+            if (temp >= 6)
+            {
+                temp = 0;
+                score++;
+                //spawner.Spawn();
+            }
+        }
+    }
+}
