b0254294ecc922ffb9109bfa1ccf9718b9ed214b
Red Nur <jw3345@drexel.edu>
2020-05-21 16:10:24 -0400

    made behavior tree

---
+++ Assets/_Scripts/BehaviorTree/BehaviorTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    class BehaviorTree : MonoBehaviour
+    {
+        public Composite root;
+
+        void Update()
+        {
+            root.Run();
+
+            State currentState = root.GetState();
+            if (currentState == State.Success)
+            {
+                root.Reset();
+            }
+        }
+    }
+}
