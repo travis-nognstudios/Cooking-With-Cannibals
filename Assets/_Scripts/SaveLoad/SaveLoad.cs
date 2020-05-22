c6999154927a23b0987414451a13ab85070bd594
Jason <jwang11211@gmail.com>
2020-05-22 16:50:34 -0400

    save and load

---
+++ Assets/_Scripts/SaveLoad/SaveLoad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+public class SaveLoad : MonoBehaviour
+{
+    public static void Save<T>(T objectToSave, string key)
+    {
+        string path = Application.persistentDataPath + "/save/";
+        Directory.CreateDirectory(path);
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream fileStream = new FileStream(path + key + ".dat", FileMode.Create))
+        {
+            formatter.Serialize(fileStream, objectToSave);
+        }
+    }
+
+    public static T Load<T>(string key)
+    {
+        string path = Application.persistentDataPath + "/save/";
+        BinaryFormatter formatter = new BinaryFormatter();
+        T returnValue = default(T);
+        using (FileStream fileStream = new FileStream(path + key + ".dat", FileMode.Open))
+        {
+            returnValue = (T)formatter.Deserialize(fileStream);
+        }
+
+        return returnValue;
+    }
+
+    public static bool SaveExists(string key)
+    {
+        string path = Application.persistentDataPath + "/save/" + key + ".dat";
+        return File.Exists(path);
+    }
+
+    public static void DeleteSaveFiles()
+    {
+        string path = Application.persistentDataPath + "/save/";
+        DirectoryInfo dir = new DirectoryInfo(path);
+        dir.Delete();
+        Directory.CreateDirectory(path);
+    }
+}
---
+++ Assets/_Scripts/SaveLoad/SaveState.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Serving;
+using UnityEngine.SceneManagement;
+public class SaveState : MonoBehaviour
+{
+
+    public OrderSpawnerv5 orderSpawner;
+    public static bool levelOneCompleted = false;
+    public static bool levelTwoCompleted = false;
+    public static bool levelThreeCompleted = false;
+
+    private void Start()
+    {
+        
+        if (SaveLoad.SaveExists("Level One Progress"))
+        {
+            levelOneCompleted = SaveLoad.Load<bool>("Level One Progress");
+        }
+        if (SaveLoad.SaveExists("Level Two Progress"))
+        {
+            levelTwoCompleted = SaveLoad.Load<bool>("Level Two Progress");
+
+        }
+        if (SaveLoad.SaveExists("Level Three Progress"))
+        {
+            levelThreeCompleted = SaveLoad.Load<bool>("Level Three Progress");
+            Debug.Log(levelOneCompleted);
+            Debug.Log(levelTwoCompleted);
+            Debug.Log(levelThreeCompleted);
+        }
+    }
+
+    private void Update()
+    {
+        if (orderSpawner.IsServiceOver())
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                levelOneCompleted = true;
+                Save();
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                levelTwoCompleted = true;
+                Save();
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                levelThreeCompleted = true;
+                Save();
+            }
+        }
+    }
+
+    public void Save()
+    {
+        SaveLoad.Save(levelOneCompleted, "Level One Progress");
+        SaveLoad.Save(levelTwoCompleted, "Level Two Progress");
+        SaveLoad.Save(levelThreeCompleted, "Level Three Progress");
+    }
+}
