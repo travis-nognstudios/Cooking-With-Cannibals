a58ee44a60524b03d26523fe0d3c277b7be439cf
Red Nur <jw3345@drexel.edu>
2020-05-24 16:02:54 -0400

    update oil refill indicators

--- Assets/_Scripts/Cooking/OilRefill.cs
+++ Assets/_Scripts/Cooking/OilRefill.cs
@@ -9,5 +9,26 @@ namespace Cooking
     public class OilRefill : MonoBehaviour
     {
         public int refillAmount;
+        public OilRefillIndicator[] refillIndicators;
+
+        private void Start()
+        {
+            ShowRefillIndicator(refillAmount);
+        }
+
+        private void ShowRefillIndicator(int n)
+        {
+            foreach (OilRefillIndicator indicator in refillIndicators)
+            {
+                if (n == 2)
+                {
+                    indicator.ticks2.SetActive(true);
+                }
+                else if (n == 3)
+                {
+                    indicator.ticks3.SetActive(true);
+                }
+            }
+        }
     }
 }
---
+++ Assets/_Scripts/Cooking/OilRefillIndicator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LevelManagement;
+using UnityEngine.UI;
+
+namespace Cooking
+{
+    public class OilRefillIndicator : MonoBehaviour
+    {
+        public GameObject ticks2;
+        public GameObject ticks3;
+    }
+}
