4be88a719b6191f190f3ef81669f3acdf308c07a
Zachary Suarez <zjs35@drexel.edu>
2020-05-25 15:19:39 -0400

    small changes with addes colliders and trying to get light switch working

--- Assets/_Scripts/SceneObjects/FlipSwitch.cs
+++ Assets/_Scripts/SceneObjects/FlipSwitch.cs
@@ -6,32 +6,21 @@ public class FlipSwitch : MonoBehaviour
 {
 	public GameObject switchOn;
 	public GameObject switchOff;
-	private bool on;
-    // Start is called before the first frame update
-    void Start()
-    {
-        on = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-	
+	private bool isOn = false;
+    
 	private void OnTriggerEnter(Collider col)
 	{
-		if (!on)
+		if (!isOn)
 		{
 			switchOff.SetActive(false); //gameObject.disable();
 			switchOn.SetActive(true); //gameObject.enable();
-			on = true;
+			isOn = true;
 		}
 		else
 		{
 			switchOn.gameObject.SetActive(false); //disable();
 			switchOff.gameObject.SetActive(true); //enable();
-			on = false;
+			isOn = false;
 		}
 	}
 }
