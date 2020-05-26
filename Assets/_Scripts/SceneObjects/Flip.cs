86fcdb11dc101457735dd3a68c5e071b6587e878
Zachary Suarez <zjs35@drexel.edu>
2020-05-26 16:50:55 -0400

    light switch flips but still not working with object scripts

---
+++ Assets/_Scripts/SceneObjects/Flip.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public GameObject switchObj;
+	public GameObject AffectedObj;
+	public Spin script;
+	//private string objName;
+	private string scriptName;
+    private bool isTriggered = false;
+	
+	void Start()
+	{
+		//objName = AffectedObj.gameObject.name;
+		scriptName = script.gameObject.name;
+	}
+	
+	private void OnTriggerEnter(Collider col)
+	{
+		if (this.gameObject.name.Contains("_on")){
+			AffectedObj.GetComponent(Spin).setActive(false);//enabled = false; //
+			//GameObject.Find(AffectedObj.GameObject.name).GetComponent<script.gameObject.name>().setActive(false);
+			//GameObject.Find(ObjName).GetComponent(script).setActive(false);
+			Debug.Log("name contains _on");
+        }
+		else if (this.gameObject.name.Contains("_off")){
+			Debug.Log("name contains _off");
+			AffectedObj.GetComponent(Spin).setActive(true);//enabled = true; //
+			//GameObject.Find(AffectedObj.GameObject.name).GetComponent<script.gameObject.name>().setActive(true);
+			//GameObject.Find(ObjName).GetComponent(script).setActive(true);
+        }
+		else
+		{
+			Debug.Log("name contains _off");
+		}
+
+		if(!isTriggered){
+			switchObj.gameObject.SetActive(true); //gameObject.enable();//
+			this.gameObject.SetActive(false); //gameObject.disable();//
+			isTriggered = true;
+			Debug.Log("Colliding gameobject: " + col.gameObject.name);
+			Debug.Log("isTriggered = true");
+		}
+	}
+	private void OnTriggerExit(Collider col)
+    {
+        if (isTriggered)
+            isTriggered = false;
+		Debug.Log("isTriggered = false");
+    }
+}
--- Assets/_Scripts/SceneObjects/FlipSwitch.cs
+++ Assets/_Scripts/SceneObjects/FlipSwitch.cs
@@ -7,20 +7,36 @@ public class FlipSwitch : MonoBehaviour
 	public GameObject switchOn;
 	public GameObject switchOff;
 	private bool isOn = false;
+	//bool triggered = false
     
-	private void OnTriggerEnter(Collider col)
+	void Start () {
+		switchOff.SetActive(true); 
+		switchOn.SetActive(false);
+    }
+	private void OnTriggerEnter(Collider other)
 	{
-		if (!isOn)
-		{
-			switchOff.SetActive(false); //gameObject.disable();
-			switchOn.SetActive(true); //gameObject.enable();
-			isOn = true;
-		}
-		else
-		{
-			switchOn.gameObject.SetActive(false); //disable();
-			switchOff.gameObject.SetActive(true); //enable();
-			isOn = false;
-		}
+		// if (!isTriggered)
+        //{
+        //    isTriggered = true;
+			if (!isOn)
+			{
+				switchOff.SetActive(false); //gameObject.disable();
+				switchOn.SetActive(true); //gameObject.enable();
+				isOn = true;
+			}
+			else
+			{
+				switchOn.gameObject.SetActive(false); //disable();
+				switchOff.gameObject.SetActive(true); //enable();
+				isOn = false;
+			}
+		//}
 	}
+	/*
+	private void OnTriggerExit(Collider other)
+    {
+        if (isTriggered)
+            isTriggered = false;
+    }
+	*/
 }
