4ef0f597209564a365e4001663b029c6f72242bb
Zachary Suarez <zjs35@drexel.edu>
2020-05-27 02:37:07 -0400

    defib working, prefabs edited, and some colliders added

--- Assets/_Scripts/Cooking/DefibHeart.cs
+++ Assets/_Scripts/Cooking/DefibHeart.cs
@@ -7,43 +7,33 @@ namespace Cooking
     
 	public class DefibHeart : MonoBehaviour
     {
-        public ParticleSystem[] spark;
-		public int length = 6;
-		/*public int length = spark.Length;
+		public GameObject sparks;
+
 		
 		public void Start()
 		{
-			for(int i=0; i<length; i++)
-			{
-				spark[i].Stop();
-			}
-			
+			sparks.SetActive(false);
+	
 		}
-		*/
+		
         public void OnTriggerEnter(Collider other)
         {
-			for(int i=0; i<length; i++)
-					{
-						spark[i].Play();
-					}
+			
             if (other.CompareTag("Heart"))
             {
+				sparks.SetActive(true);
+				
                 if (other.GetComponent< Cookablev2 >())
                 {
-                    
-                    other.GetComponent<Cookablev2>().MakeCooked(CookType.Grill);
-                    
+                    other.GetComponent<Cookablev2>().MakeCooked(CookType.Grill);  
                 }
             }
         }
 		
 		public void OnTriggerExit(Collider other)
         {
-			for(int i=0; i<length; i++)
-					{
-						spark[i].Stop();
-					}
+			sparks.SetActive(false);
 		}
-
+		
     }
 }
