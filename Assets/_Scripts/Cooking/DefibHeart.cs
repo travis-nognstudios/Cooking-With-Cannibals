cd3ce2b87b2cb98d88e10729de37de171c994b40
Zachary Suarez <zjs35@drexel.edu>
2020-05-27 00:11:29 -0400

    working on light switches

--- Assets/_Scripts/Cooking/DefibHeart.cs
+++ Assets/_Scripts/Cooking/DefibHeart.cs
@@ -7,8 +7,9 @@ namespace Cooking
     
 	public class DefibHeart : MonoBehaviour
     {
-        static public ParticleSystem[] spark;
-		static public int length = spark.Length;
+        public ParticleSystem[] spark;
+		public int length = 6;
+		/*public int length = spark.Length;
 		
 		public void Start()
 		{
@@ -18,19 +19,20 @@ namespace Cooking
 			}
 			
 		}
-	
+		*/
         public void OnTriggerEnter(Collider other)
         {
+			for(int i=0; i<length; i++)
+					{
+						spark[i].Play();
+					}
             if (other.CompareTag("Heart"))
             {
                 if (other.GetComponent< Cookablev2 >())
                 {
                     
                     other.GetComponent<Cookablev2>().MakeCooked(CookType.Grill);
-                    for(int i=0; i<length; i++)
-					{
-						spark[i].Play();
-					}
+                    
                 }
             }
         }
--- Assets/_Scripts/SceneObjects/Flip.cs
+++ Assets/_Scripts/SceneObjects/Flip.cs
@@ -6,28 +6,28 @@ public class Flip : MonoBehaviour
 {
     public GameObject switchObj;
 	public GameObject AffectedObj;
-	public Spin script;
+	//public Spin script;
 	//private string objName;
-	private string scriptName;
+	//private string scriptName;
     private bool isTriggered = false;
 	
 	void Start()
 	{
 		//objName = AffectedObj.gameObject.name;
-		scriptName = script.gameObject.name;
+		//scriptName = script.gameObject.name;
 	}
 	
 	private void OnTriggerEnter(Collider col)
 	{
 		if (this.gameObject.name.Contains("_on")){
-			AffectedObj.GetComponent(Spin).setActive(false);//enabled = false; //
+			//AffectedObj.GetComponent(Spin).setActive(false);//enabled = false; //
 			//GameObject.Find(AffectedObj.GameObject.name).GetComponent<script.gameObject.name>().setActive(false);
 			//GameObject.Find(ObjName).GetComponent(script).setActive(false);
 			Debug.Log("name contains _on");
         }
 		else if (this.gameObject.name.Contains("_off")){
 			Debug.Log("name contains _off");
-			AffectedObj.GetComponent(Spin).setActive(true);//enabled = true; //
+			//AffectedObj.GetComponent(Spin).setActive(true);//enabled = true; //
 			//GameObject.Find(AffectedObj.GameObject.name).GetComponent<script.gameObject.name>().setActive(true);
 			//GameObject.Find(ObjName).GetComponent(script).setActive(true);
         }
