bb5c368b61d8ba1002e156af09bf2881a0fb7b6d
Red Nur <jw3345@drexel.edu>
2020-05-30 14:24:41 -0400

    knife hit reaction

---
+++ Assets/_Scripts/AI/Hero/HeroAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using BehaviorTree;
+
+namespace AI
+{
+    class HeroAnimation : Node
+    {
+        [Header("Reaction")]
+        public Animator animator;
+        public string triggerName;
+
+        private State myState;
+       
+        void Start()
+        {
+
+        }
+
+        public override State GetState()
+        {
+            return myState;
+        }
+
+        public override void Run()
+        {
+            animator.SetTrigger(triggerName);
+            myState = State.Success;
+        }
+
+        
+    }
+}
---
+++ Assets/_Scripts/AI/Hero/ReactToKnifeThrow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using BehaviorTree;
+using Serving;
+
+namespace AI
+{
+    class ReactToKnifeThrow : Node
+    {
+        private bool alreadyReacted;
+
+        private State myState;
+
+        public override State GetState()
+        {
+            return myState;
+        }
+
+        public override void Run()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyReacted)
+            {
+                if (other.CompareTag("Knife"))
+                {
+                    Debug.Log("You threw a knife at me jafeel");
+                    alreadyReacted = true;
+                    myState = State.Success;
+                }
+            }
+            else
+            {
+                myState = State.Failure;
+            }
+        }
+    }
+}
