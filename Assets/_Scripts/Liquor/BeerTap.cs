be0ce181943b8fc1160b48c74f020c7c4b6689a2
Red Nur <jw3345@drexel.edu>
2020-05-30 23:35:14 -0400

    update and fix stuff

---
+++ Assets/_Scripts/Liquor/BeerTap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Liquor
+{
+    class BeerTap : MonoBehaviour
+    {
+        public GameObject beerArea;
+        public ParticleSystem beerFX;
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("BeerTapHandle"))
+            {
+                TurnOnBeer();
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("BeerTapHandle"))
+            {
+                TurnOffBeer();
+            }
+        }
+
+        void TurnOnBeer()
+        {
+            beerArea.SetActive(true);
+            beerFX.Play();
+        }
+
+        void TurnOffBeer()
+        {
+            beerArea.SetActive(false);
+            beerFX.Stop();
+        }
+    }
+}
