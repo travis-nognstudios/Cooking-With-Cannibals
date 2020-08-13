32de2bda206d7d536963c037b1f08c3dc0a33489
Jason <jwang11211@gmail.com>
2020-08-13 04:58:14 -0400

    level saves and oil refill fix

---
+++ Assets/_Scripts/SaveLoad/MainMenuLock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLock : MonoBehaviour
+{
+    public GameObject LevelOneLogo;
+    public GameObject LevelTwoLogo;
+    public GameObject LevelThreeLogo;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        if (SaveState.levelOneCompleted == false)
+        {
+            LevelTwoLogo.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            LevelTwoLogo.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (SaveState.levelTwoCompleted == false)
+        {
+            LevelThreeLogo.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            LevelThreeLogo.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+
+}
