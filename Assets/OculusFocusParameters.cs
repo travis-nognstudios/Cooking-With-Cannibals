33c128cfc0b6659a357f11683fb09d1476f0f76e
Travis Hove <thove@nognstudios.com>
2020-10-28 17:22:21 +0000

    Attempting Oculus Focus scripts

---
+++ Assets/OculusFocusParameters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusFocusParameters : MonoBehaviour
+{
+    public GameObject rHand;
+    public GameObject lHand;
+
+    private void Start()
+    {
+        rHand = GameObject.Find("hand_right_renderPart_0");
+
+        lHand = GameObject.Find("hand_left_renderPart_0");
+    }
+
+    void Update()
+    {
+       if (OVRManager.hasInputFocus)
+        {
+            Debug.Log("The player should be in game and accepting input");
+            rHand.GetComponent<Renderer>().enabled = true;
+            lHand.GetComponent<Renderer>().enabled = true;
+        }
+
+       if (!OVRManager.hasInputFocus)
+        {
+            Debug.Log("The player hit the dash button. No input should be processing at this time");
+            rHand.GetComponent<Renderer>().enabled = false;
+            lHand.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
