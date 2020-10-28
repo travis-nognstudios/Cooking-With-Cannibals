4ea8bdcd52a0b47e721d320d1b245c33f8af7cf7
Travis Hove <thove@nognstudios.com>
2020-10-28 08:18:12 +0000

    Made NPC hero character talk, but need to fix Bruce rig for him to work

---
+++ Assets/TalkingNPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingNPC : MonoBehaviour
+{
+    public GameObject talkingJoint;
+    public AudioSource characterVoice;
+    public bool xAxis = false;
+    public bool zAxis = false;
+    public float jawRotx;
+    public float jawRotz;
+    public float jawWidth;
+    public float talkSpeed;
+
+    void Awake()
+    {
+        jawRotx = talkingJoint.transform.rotation.x;
+        jawRotz = talkingJoint.transform.rotation.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterVoice.isPlaying && xAxis)
+        {
+            talkingJoint.transform.localRotation = Quaternion.Euler((jawRotx + Mathf.Sin(Time.time * talkSpeed) * jawWidth), 0, 0);
+        }
+        else if (characterVoice.isPlaying && zAxis)
+        {
+            talkingJoint.transform.localRotation = Quaternion.Euler(0, 0, (jawRotz + Mathf.Sin(Time.time * talkSpeed) * jawWidth));
+        }
+    }
+}
